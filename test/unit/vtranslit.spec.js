035d49c8aaa0905f949a62930abc292c6f3e1936
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-01-16 17:49:10 +0530

    add tests for conjunct consonants.

--- test/unit/vtranslit.spec.js
+++ test/unit/vtranslit.spec.js
@@ -3,9 +3,21 @@ import { vtranslit } from '../../src';
 
 describe('vtranslit', () => {
 
-  it('should transliterate ` ` to ` `', () => {
+  it('should transliterate to ` ` for ` `', () => {
 
     expect(vtranslit(' ')).to.equal(' ');
+
+  });
+
+  it('should transliterate to `प्र` for `pra`', () => {
+
+    expect(vtranslit('pra')).to.equal('प्र');
+
+  });
+
+  it('should transliterate to `क्षेत्रज्ञ` for `kShetraj~na`', () => {
+
+    expect(vtranslit('kShetraj~na')).to.equal('क्षेत्रज्ञ');
 
   });
 
