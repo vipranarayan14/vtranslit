134bf3fef7d7d8b882550a6aff2325155c044e3b
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-01-16 06:35:56 +0530

    change shemes from JSON to JS for easy access and usage.

---
+++ src/vtranslit-schemes/vtranslit-itrans-scheme.js
@@ -0,0 +1,29 @@
+export const itransScheme = {
+  'alternates': {
+    'A': 'aa',
+    'I': 'ii',
+    'LLI': 'L^I',
+    'LLi': 'L^i',
+    'OM': 'AUM',
+    'RRI': 'R^I',
+    'RRi': 'R^i',
+    'U': 'uu',
+  },
+  'consonants': [
+    'k', 'kh', 'g', 'gh', '~N',
+    'ch', 'Ch', 'j', 'jh', '~n',
+    'T', 'Th', 'D', 'Dh', 'N',
+    't', 'th', 'd', 'dh', 'n',
+    'p', 'ph', 'b', 'bh', 'm',
+    'y', 'r', 'l', 'v',
+    'sh', 'Sh', 's', 'h',
+    'L'
+  ],
+  'defaultVowel': 'a',
+  'symbols': [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.a', '|', '||', 'OM'
+  ],
+  'virama': [''],
+  'vowelMarks': ['a', 'A', 'i', 'I', 'u', 'U', 'RRi', 'RRI', 'LLi', 'LLI', 'e', 'ai', 'o', 'au', '.m', '.h'],
+  'vowels': ['a', 'A', 'i', 'I', 'u', 'U', 'RRi', 'RRI', 'LLi', 'LLI', 'e', 'ai', 'o', 'au']
+};
