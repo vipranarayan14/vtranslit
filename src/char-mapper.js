cfa70f0be73f665388e63d19c7f3cccc7f1045c6
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-01-17 16:24:17 +0530

    rearrange functions to alphabetically and to avoid 'hoisting'. make 'mapVowels', 'mapConsonants' and 'mapSymbols' function to iterate over array of each char to support alternate chars. remove unnecessay 'mapAlternates' function as the other functions themselves do the work.

--- src/char-mapper.js
+++ src/char-mapper.js
@@ -1,3 +1,46 @@
+const mapConsonants = (charMap, fromScheme, toScheme) => {
+
+  fromScheme.consonants.forEach((consonant, letterIndex) => {
+
+    fromScheme.vowelMarks.forEach((mark, markIndex) => {
+
+      mark.forEach(alternateMark => {
+
+        consonant.forEach(alternateConsonant => {
+
+          charMap[`${alternateConsonant}${alternateMark}`] =
+            `${toScheme.consonants[letterIndex]}${toScheme.vowelMarks[markIndex][0]}`;
+
+        });
+
+      });
+
+    });
+
+  });
+
+};
+
+const mapSymbols = (charMap, fromScheme, toScheme) => {
+
+  fromScheme.symbols.forEach((symbol, index) => {
+
+    symbol.forEach(alternateSymbol => charMap[alternateSymbol] = toScheme.symbols[index][0]);
+
+  });
+
+};
+
+const mapVowels = (charMap, fromScheme, toScheme) => {
+
+  fromScheme.vowels.forEach((vowel, index) => {
+
+    vowel.forEach(alternateVowel => charMap[alternateVowel] = toScheme.vowels[index][0]);
+
+  });
+
+};
+
 export const mapChars = (fromScheme, toScheme) => {
 
   const charMap = {};
@@ -8,60 +51,8 @@ export const mapChars = (fromScheme, toScheme) => {
 
   mapSymbols(charMap, fromScheme, toScheme);
 
-  mapAlternates(charMap, fromScheme);
-
   charMap[' '] = ' ';
 
   return charMap;
-
-};
-
-const mapSymbols = (charMap, fromScheme, toScheme) => {
-
-  fromScheme.symbols.forEach((symbol, index) => {
-
-    charMap[symbol] = toScheme.symbols[index];
-
-  });
-
-};
-
-const mapConsonants = (charMap, fromScheme, toScheme) => {
-
-  fromScheme.consonants.forEach((consonant, letterIndex) => {
-
-    fromScheme.vowelMarks.forEach((mark, markIndex) => {
-
-      charMap[`${consonant}${mark}`] = `${toScheme.consonants[letterIndex]}${toScheme.vowelMarks[markIndex]}`;
-
-    });
-
-  });
-
-};
-
-const mapVowels = (charMap, fromScheme, toScheme) => {
-
-  fromScheme.vowels.forEach((vowel, index) => {
-
-    charMap[vowel] = toScheme.vowels[index];
-
-  });
-
-};
-
-const mapAlternates = (charMap, fromScheme) => {
-
-  if (!fromScheme.alternates) {
-
-    return;
-
-  }
-
-  Object.keys(fromScheme.alternates).map(alternate => {
-
-    charMap[fromScheme.alternates[alternate]] = charMap[alternate];
-
-  });
 
 };
