9204fdc90ed2fc103a948b287d2a3ef0e10a20de
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-03-29 13:57:35 +0530

    split the 'contains characters' test into 3 tests for vowels, consonants, vowelMarks.

--- test/unit/make-scheme-tree.spec.js
+++ test/unit/make-scheme-tree.spec.js
@@ -9,19 +9,37 @@ const state = {
 
 };
 
-describe('makeFromSchemeTree', () => {
+describe('fromSchemeTree', () => {
 
   const fromSchemeTree = makeFromSchemeTree('Itran', state);
 
-  it('should return an object literal', () => {
+  it('should be an object literal', () => {
 
     expect(fromSchemeTree).to.be.an('object');
 
   });
 
-  it('should contain all of these - [`A`, `aa`, `RRI`, `|`, `p`, `~n`]', () => {
+  it('should contain all of these vowels - [`*A`, `*aa`, `*U`, `*RRi`, `*R^i`, `*au`]', () => {
 
-    expect(fromSchemeTree).to.contain.all.keys(['A', 'aa', 'RRI', '|', 'p', '~n']);
+    expect(fromSchemeTree).to.contain.all.keys(['*A', '*aa', '*U', '*RRi', '*R^i', '*au']);
+
+  });
+
+  it('should contain all of these consonants - [`k`, `~n`, `N^`, `Dh`, `y`, `L`, `w`, `shh`]', () => {
+
+    expect(fromSchemeTree).to.contain.all.keys(['k', '~n', 'N^', 'Dh', 'y', 'L', 'w', 'shh']);
+
+  });
+
+  it('should contain all of these vowelMarks - [`A`, `aa`, `U`, `RRi`, `R^i`, `au`]', () => {
+
+    expect(fromSchemeTree).to.contain.all.keys(['A', 'aa', 'U', 'RRi', 'R^i', 'au']);
+
+  });
+
+  it('should contain all of these symbols - [`0`, `4`, `|`, `.a`, `.m`]', () => {
+
+    expect(fromSchemeTree).to.contain.all.keys(['0', '4', '|', '.a', '.m']);
 
   });
 
