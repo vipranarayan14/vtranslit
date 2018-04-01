d9e0898f5753076a2359afc8f7129883b1a23118
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-01 14:27:05 +0530

    add support for Taml scheme.

--- src/get-scheme.js
+++ src/get-scheme.js
@@ -1,9 +1,11 @@
 import { devanagariScheme } from './vtranslit-schemes/vtranslit-Deva-scheme';
 import { itransScheme } from './vtranslit-schemes/vtranslit-Itrn-scheme';
+import { tamilScheme } from './vtranslit-schemes/vtranslit-Taml-scheme';
 
 export const getScheme = schemeCode =>
 
   [
+    itransScheme,
     devanagariScheme,
-    itransScheme
+    tamilScheme
   ].find(scheme => scheme.about.scriptCode === schemeCode);
