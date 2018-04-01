48c89ce3d7064d272f5523aeea0a810aa5ab039a
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-01 08:35:46 +0530

    add support for exporting 'vtranslit' as library for browser.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
-const path = require('path');
-const filename = 'vtranslit.js';
+const libraryName = 'vTranslit';
 
-module.exports = {
-  entry: './src',
+const path = require('path');
+const filename = 'vtranslit';
+
+const baseConfig = {
+  entry: './src/index.js',
   module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader'
+    loaders: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['babel-preset-env']
         }
       }
-    ]
-  },
-  output: {
-    filename,
-    library: 'vtranslit',
-    libraryTarget: 'umd',
-    path: path.join(__dirname, 'dist'),
+    }]
   }
 };
+
+const browserConfig = Object.assign({}, baseConfig, {
+
+  output: {
+    filename: `${filename}.js`,
+    library: libraryName,
+    libraryExport: libraryName,
+    libraryTarget: 'window',
+    path: path.join(__dirname, 'dist')
+  }
+
+});
+
+const npmConfig = Object.assign({}, baseConfig, {
+
+  output: {
+    filename: `${filename}.npm.js`,
+    library: libraryName,
+    libraryTarget: 'umd',
+    path: path.join(__dirname, 'dist')
+  }
+
+});
+
+module.exports = [
+  browserConfig,
+  npmConfig
+];
