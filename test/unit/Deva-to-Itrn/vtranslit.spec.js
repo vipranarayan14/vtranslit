995e2c945efc709bd628ae66bde7fc6c35872692
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-02 15:42:17 +0530

    replace file test from simple tests as the former does not seem easy to do.

--- test/unit/Deva-to-Itrn/vtranslit.spec.js
+++ test/unit/Deva-to-Itrn/vtranslit.spec.js
@@ -1,30 +1,105 @@
 import { expect } from 'chai';
 import { vTranslit } from '../../../src/index';
 
-// const fs = require('fs');
-
 describe('vtranslit from Deva to Itrn', () => {
 
   const vt = vTranslit('Deva', 'Itrn');
 
-  it('should transliterate `वाक् इव` to `vAk iva`', () => {
+  it('should not transliterate `#$%&*!$%()^&^$#&(=+-` and just return them', () => {
 
-    expect(vt('वाक् इव')).to.equal('vAk iva');
+    expect(vt('#$%&*!$%()^&^$#&(=+-')).to.equal('#$%&*!$%()^&^$#&(=+-');
 
   });
 
-  // it('should transliterate a given data from file', () => {
+  it('should transliterate ` ` to ` `', () => {
 
-  //   fs.readFile('files/test-deva-to-itrn.txt', 'utf8', (inputError, inputData) => {
+    expect(vt(' ')).to.equal(' ');
 
-  //     fs.readFile('files/test-itrn-to-deva.txt', 'utf8', (outputError, outputData) => {
+  });
 
-  //       expect(vt(inputData)).to.equal(outputData);
+  it('should transliterate `ॐ` to `OM`', () => {
 
-  //     });
+    expect(vt('ॐ')).to.equal('OM');
 
-  //   });
+  });
 
-  // });
+  it('should transliterate `ऽ` to `.a`', () => {
+
+    expect(vt('ऽ')).to.equal('.a');
+
+  });
+
+  it('should transliterate `श्री` to `shrI`', () => {
+
+    expect(vt('श्री')).to.equal('shrI');
+
+  });
+
+  it('should transliterate `अ` to `a`', () => {
+
+    expect(vt('अ')).to.equal('a');
+
+  });
+
+  it('should transliterate `ख` to `kha`', () => {
+
+    expect(vt('ख')).to.equal('kha');
+
+  });
+
+  it('should not transliterate `ङ्` to `N^`', () => {
+
+    expect(vt('ङ्')).to.not.equal('N^');
+
+  });
+
+  it('should transliterate `षीं` to `ShIM`', () => {
+
+    expect(vt('षीं')).to.equal('ShIM');
+
+  });
+
+  it('should transliterate `प्र` to `pra`', () => {
+
+    expect(vt('प्र')).to.equal('pra');
+
+  });
+
+  it('should transliterate `अ इ उ ण्` to `a i u N`', () => {
+
+    expect(vt('अ इ उ ण्')).to.equal('a i u N');
+
+  });
+
+  it('should transliterate `॥ १-१ ॥` to `|| 1-1 ||`', () => {
+
+    expect(vt('॥ १-१ ॥')).to.equal('|| 1-1 ||');
+
+  });
+
+  it('should transliterate `क्षेत्रज्ञ` to `kShetraj~na`', () => {
+
+    expect(vt('क्षेत्रज्ञ')).to.equal('kShetraj~na');
+
+  });
+
+  it('should transliterate given phrase', () => {
+
+    expect(vt('गीतागोपालौ औषधार्थं वनम् एति ।'))
+      .to.equal('gItAgopAlau auShadhArthaM vanam eti |');
+
+  });
+
+  it('should transliterate the given verse', () => {
+
+    expect(vt(
+      'वागर्थाविव संपृक्तौ वागर्थप्रतिपत्तये ।' +
+      'जगतः पितरौ वन्दे पार्वतीपरमेश्वरौ ॥'
+    )).to.equal(
+      'vAgarthAviva saMpRRiktau vAgarthapratipattaye |' +
+      'jagataH pitarau vande pArvatIparameshvarau ||'
+    );
+
+  });
 
 });
