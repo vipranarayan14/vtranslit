9551485d7690ec41ff655bcd99f18d4310c6d233
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-18 13:49:42 +0530

    export schemes: trail 2.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import { getAvailableSchemes, getScheme } from './schemes';
 import { devanagariScheme } from './vtranslit-schemes/vtranslit-Deva-scheme';
 import { itransScheme } from './vtranslit-schemes/vtranslit-Itrn-scheme';
 import { kannadaScheme } from './vtranslit-schemes/vtranslit-Knda-scheme';
 import { tamilScheme } from './vtranslit-schemes/vtranslit-Taml-scheme';
 import { teluguScheme } from './vtranslit-schemes/vtranslit-Telu-scheme';
 
-export const vTranslitSchemes = {
+module.exports = {
 
   devanagariScheme,
-  getAvailableSchemes,
-  getScheme,
   itransScheme,
   kannadaScheme,
   tamilScheme,
