8008376fbde9b8b6dbd99d749b5f15d38407ada0
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-23 23:18:30 +0530

    export 'findScheme'.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { findScheme } from './find-scheme';
 import { getAvailableSchemes } from './get-available-schemes';
 import { getScheme } from './get-scheme';
 import { schemes } from './schemes';
 
 export const vTranslitSchemes = {
 
+  findScheme,
   getAvailableSchemes,
   getScheme,
   schemes
