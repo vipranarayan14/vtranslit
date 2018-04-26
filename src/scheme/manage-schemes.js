eb0c059314a8340e42825bbf6c8c1e9921fa345f
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-26 14:58:53 +0530

    move 'schemes.js' to 'scheme/' and rename to 'manage-schemes.js'.

---
+++ src/scheme/manage-schemes.js
@@ -0,0 +1,15 @@
+import { findScheme } from './find-scheme';
+import { getScheme } from './get-scheme';
+import { listAvailableSchemes } from './list-available-schemes';
+
+export const manageSchemes = schemes => (
+
+  {
+
+    find: findScheme(schemes),
+    get: getScheme(schemes),
+    list: listAvailableSchemes(schemes)
+
+  }
+
+);
