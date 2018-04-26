fa6427fc427cbe6672e96b033ee704923c2917c8
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-26 13:00:18 +0530

    modify test to reflect the current usage.

--- test/unit/find-scheme.spec.js
+++ test/unit/find-scheme.spec.js
@@ -1,53 +1,60 @@
 import { expect } from 'chai';
 import { findScheme } from '../../src/find-scheme';
+import vTranslitDevaScheme from 'vtranslit-deva-scheme';
+import vTranslitItrnScheme from 'vtranslit-itrn-scheme';
+
+const find = findScheme([
+  vTranslitDevaScheme,
+  vTranslitItrnScheme
+]);
 
 describe('findScheme', () => {
 
   it('should return scheme code as `Itrn` for the given string', () => {
 
-    expect(findScheme('viSNu')).to.equal('Itrn');
+    expect(find('viSNu')).to.equal('Itrn');
 
   });
 
   it('should return scheme code as `Deva` for the given string', () => {
 
-    expect(findScheme('विष्णु')).to.equal('Deva');
+    expect(find('विष्णु')).to.equal('Deva');
 
   });
 
   it('should return scheme code as `Itrn` for the given mixed string', () => {
 
-    expect(findScheme('nalinI दल')).to.equal('Itrn');
+    expect(find('nalinI दल')).to.equal('Itrn');
 
   });
 
   it('should return scheme code as `Deva` for the given mixed string', () => {
 
-    expect(findScheme('नलिनी dala')).to.equal('Deva');
+    expect(find('नलिनी dala')).to.equal('Deva');
 
   });
 
   it('should return scheme code as `Itrn` for the given equally mixed string', () => {
 
-    expect(findScheme('kumuदिनी')).to.equal('Itrn');
+    expect(find('kumuदिनी')).to.equal('Itrn');
 
   });
 
   it('should return scheme code as `Deva` for the given equally mixed string', () => {
 
-    expect(findScheme('कुमुdinI')).to.equal('Deva');
+    expect(find('कुमुdinI')).to.equal('Deva');
 
   });
 
   it('should return scheme code as `Itrn` for the given mixed string', () => {
 
-    expect(findScheme('rAmaH vanaM गच्छति । तत्र सीता सह निवसति ।')).to.equal('Itrn');
+    expect(find('rAmaH vanaM गच्छति । तत्र सीता सह निवसति ।')).to.equal('Itrn');
 
   });
 
   it('should return scheme code as `Deva` for the given equally mixed string', () => {
 
-    expect(findScheme('रामः वनं gachChati | tatra sItA saha nivasati |')).to.equal('Deva');
+    expect(find('रामः वनं gachChati | tatra sItA saha nivasati |')).to.equal('Deva');
 
   });
 
