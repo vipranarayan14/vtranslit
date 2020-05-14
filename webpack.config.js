94642728d864ca4a03042535d90ee1d26d779f4b
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-05-14 09:42:29 +0530

    chore: update '@babel/preset-env' pkg reference

--- webpack.config.js
+++ webpack.config.js
@@ -6,43 +6,38 @@ const filename = 'vtranslit';
 const baseConfig = {
   entry: './src/index.js',
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      test: /\.js$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['babel-preset-env']
-        }
-      }
-    }]
-  }
+    loaders: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
 
 const browserConfig = Object.assign({}, baseConfig, {
-
   output: {
     filename: `${filename}.js`,
     library: libraryName,
     libraryExport: libraryName,
     libraryTarget: 'window',
-    path: path.join(__dirname, 'dist')
-  }
-
+    path: path.join(__dirname, 'dist'),
+  },
 });
 
 const npmConfig = Object.assign({}, baseConfig, {
-
   output: {
     filename: `${filename}.npm.js`,
     library: libraryName,
     libraryTarget: 'umd',
-    path: path.join(__dirname, 'dist')
-  }
-
+    path: path.join(__dirname, 'dist'),
+  },
 });
 
-module.exports = [
-  browserConfig,
-  npmConfig
-];
+module.exports = [browserConfig, npmConfig];
